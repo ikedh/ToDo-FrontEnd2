fd1d29332bb26412c46b78c3d7bde7cc56ca3d6c
ikedh <mcalderon@digitalhouse.com>
2023-09-08 21:16:58 -0300

    implementando Singup

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,58 +1,71 @@
 window.addEventListener('load', function () {
     /* ---------------------- obtenemos variables globales ---------------------- */
-        const form = document.forms[0];
-        const inputNombre = document.getElementById('inputNombre');
-        const inputApellido = document.getElementById('inputApellido');
-        const inputEmail = document.getElementById('inputEmail');
-        const inputPassword = document.getElementById('inputPassword');
-        const inputPasswordRepetida = document.getElementById('inputPasswordRepetida');
-        const url = "https://todo-api.ctd.academy/v1"
-
-
+    const form = document.forms[0];
+    const nombre = document.querySelector('#inputNombre');
+    const apellido = document.querySelector('#inputApellido');
+    const email = document.querySelector('#inputEmail');
+    const password = document.querySelector('#inputPassword');
+    const url = 'https://todo-api.ctd.academy/v1';
 
     /* -------------------------------------------------------------------------- */
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
     form.addEventListener('submit', function (event) {
         event.preventDefault();
-
+        //creamos el cuerpo de la request
         const payload = {
-            firstName: inputNombre.value,
-            lastName: inputApellido.value,
-            email: inputEmail.value,
-            password: inputPassword.value,
-        }
-
+            firstName: nombre.value,
+            lastName: apellido.value, 
+            email: email.value,
+            password: password.value
+        };
+        //configuramos la request del Fetch
         const settings = {
-            method : 'POST',
+            method: 'POST',
             body: JSON.stringify(payload),
-            headers:{
-                'Content-type': 'application-json'
-            }  
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        //lanzamos la consulta de login a la API
+        realizarRegister(settings);
 
-        }
-
-        realizarRegister(settings)
-        console.log(payload);
-        
+        //limpio los campos del formulario
+        form.reset();
     });
 
     /* -------------------------------------------------------------------------- */
     /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
     /* -------------------------------------------------------------------------- */
     function realizarRegister(settings) {
+        console.log("Lanzando la consulta a la API");
+        fetch(`${url}/users`, settings)
+            .then(response => {
+                console.log(response);
 
-        fetch(`${url}/users`,settings)
-        .then(response =>{
-            console.log(response);
-        })
-        if(!response.ok){
-            alert("Todo mal")
-        }
+                if (response.ok != true) {
+                    alert("Alguno de los datos es incorrecto.")
+                }
 
+                return response.json();
 
+            })
+            .then(data => {
+                console.log("Promesa cumplida:");
+                console.log(data);
 
+                if (data.jwt) {
+                    //guardo en LocalStorage el objeto con el token
+                    localStorage.setItem('jwt', JSON.stringify(data.jwt));
 
+                    //redireccionamos a la página
+                    location.replace('./mis-tareas.html');
+                }
+                
+            }).catch(err => {
+                console.log("Promesa rechazada:");
+                console.log(err);
+            })
     };
 
 
